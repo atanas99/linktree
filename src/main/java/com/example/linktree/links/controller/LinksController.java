1e08c3cb537980defaeca0c14cfb0582bae8b17e
atanas99 <atanas.marinov19@gmail.com>
2025-02-21 12:29:33 +0100

    add CrossOrigin

--- src/main/java/com/example/linktree/links/controller/LinksController.java
+++ src/main/java/com/example/linktree/links/controller/LinksController.java
@@ -19,7 +19,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/links")
-@CrossOrigin()
+@CrossOrigin(origins = "http://localhost:3033")
 @RequiredArgsConstructor
 public class LinksController {
 
--- src/main/java/com/example/linktree/session/controller/SessionController.java
+++ src/main/java/com/example/linktree/session/controller/SessionController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/session")
-@CrossOrigin()
+@CrossOrigin(origins = "http://localhost:3033")
 public class SessionController {
 
     private final SessionService service;
