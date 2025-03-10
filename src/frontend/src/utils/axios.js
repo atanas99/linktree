81224ae38971a685949d1e0ee4c0e8f2c6772c6f
atanas99 <atanas.marinov19@gmail.com>
2025-03-10 14:25:27 +0100

    add Amount of users

--- src/frontend/src/utils/axios.js
+++ src/frontend/src/utils/axios.js
@@ -45,6 +45,7 @@ export const endpoints = {
     getUser: (username) => `/users/get/byEmail/${username}`,
     updateUser: '/users/update',
     getUserById: (userId) => `/users/get/byId/${userId}`,
+    getAmoutOfUsers: '/users/all',
   },
   products: {
     createProduct: (userId) => `/products/create/byUser/${userId}`,
